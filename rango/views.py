2b4fe01995d4e95d8e3e571d184377fdab66b50b
Stewart Brown <stewartbrown@glaroam2-146-21.wireless.gla.ac.uk>
2017-01-19 14:47:43 +0000

    near end chapter 4

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
-#from django.shortcuts import render#
-
-
+from django.shortcuts import render
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says alright Roaster")
+    context_dict = {'boldmessage': "Rango you total roaster"}
 
+    #return HttpResponse("Rango says give me a biscuit <br/> <a href='/rango/about/'>About</a>")
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    return HttpResponse("This is the about page <br/> <a href='/rango/'>Home</a>")
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -16,13 +16,17 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
-from rango import views
+#from rango import views
 
 from rango import views
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^admin/', include('rango.urls')),
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
