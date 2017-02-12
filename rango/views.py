0f11d86b440f7ac30867b9fb3c1605b457330ff6
Stewart Brown <stewartbrown@Stewart-Browns-MacBook.local>
2017-02-12 01:58:32 +0000

    end of chapter 10- clarify last task - in particular prob with line 58 'visitor_cookie_handler(request)'

--- rango/views.py
+++ rango/views.py
@@ -18,9 +18,33 @@ from rango.forms import CategoryForm
 
 from rango.forms import PageForm
 
+from datetime import datetime
 
 
+#helper method
+def get_server_side_cookie(request,cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+        return val
 
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits','1'))
+
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], "%Y-%m-%d %H:%M:%S")
+
+        # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).seconds > 0:
+        visits = visits + 1
+        # update the last visit cookie now that we have updated the count response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+            # set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+            # Update/set the visits cookie
+    request.session['visits'] = visits
 
 
 def index(request):
@@ -31,8 +55,12 @@ def index(request):
 
     context_dict = {'categories': category_list, 'pages': page_list}
 
-    #return HttpResponse("Rango says give me a biscuit <br/> <a href='/rango/about/'>About</a>")
-    return render(request, 'rango/index.html', context=context_dict)
+    #visitor_cookie_handler(request)
+
+    context_dict['visits']=request.session['visits']
+    print(request.session['visits'])
+    response = render(request, 'rango/index.html', context_dict)
+    return response
 
 def about(request):
     if request.session.test_cookie_worked():
@@ -173,6 +201,7 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('index'))
+
 
 
 
